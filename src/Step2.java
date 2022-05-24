2cc216fc50c82c47c5275163100c67681bf94a0a
Your Name <you@example.com>
2022-05-24 20:41:11 +0600

    Second commit

--- src/Step2.java
+++ src/Step2.java
@@ -3,23 +3,10 @@ public class Step2 {
         int a = 100;
         int b = a + 200;
         int c = a + 1300;
-        boolean thousand = true;
 
         int bonus;
-        if (thousand) ;
-        {
-            bonus = 14;
-        }
-        if (false)
-        {
-            bonus = 0;
-        }
-        {
-            System.out.println("Итог без бонуса=" + b);
-        }
-        {
-            System.out.println("Итог с бонусом=" + (c + bonus));
-        }
-
+        bonus = 14;
+        System.out.println("Итог без бонуса=" + b);
+        System.out.println("Итог с бонусом=" + (c + bonus));
     }
 }
