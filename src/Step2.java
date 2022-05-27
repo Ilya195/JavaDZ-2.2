bcd50330a1ae2c1e2c513e3b3829db0b0a8ef8ac
Your Name <you@example.com>
2022-05-27 19:58:19 +0600

    Changed Step2

--- src/Step2.java
+++ src/Step2.java
@@ -4,8 +4,11 @@ public class Step2 {
         int refill = balance + 1300;
 
         int bonus = 0;
-        if (refill >= 1000)
+        if (refill >= 1000){
             bonus = refill / 100;
+        }else {
+            bonus = 0;
+        }
 
         System.out.println("Итог с бонусом=" + (refill + bonus));
 
